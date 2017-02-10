814c67162e7ad545c27dd68d7ab0d94317439fdc
talha67 <talhajaved67@gmail.com>
2017-02-10 05:02:04 +0500

    Update FCFS.py

--- FCFS.py
+++ FCFS.py
@@ -11,24 +11,27 @@ def inputProcess( Process_array, Total_process, Total_waiting_time ):
        Process_array[i].append(int(raw_input('Burst Time: ')))
    return Total_waiting_time, Total_process
 
+
+def Waiting_time(Process_array):
+	time=0
+	for proc in Process_array:
+		time = time if time > proc[1] else proc[1]
+		time = time + proc[2]
+		proc.append(time)
+		proc.append(proc[3] - proc[2] - proc[1] if time > proc[1] else 0)
+		proc.append(proc[4] + proc[2])
+	return Process_array
+
+
 def showtabluarForm( Process_array, Total_process ):
    Process_array.sort(key = lambda Process_array:Process_array[1])
-   print 'Name\tArrivalTime\tBurstTime'
+   print 'Name\tArrivalTime\tBurstTime\tFinishTime\tWaitingTime\tTurnAroundTime'
    for i in xrange(Total_process):
-   	print Process_array[i][0],'\t\t',Process_array[i][1],'\t\t',Process_array[i][2]
-   return
-
-def PrintWaitingTime(Total_waiting_time,Total_process):
-   print 'Waiting time: ',Total_waiting_time
-   print 'Average waiting time: ',(Total_waiting_time/Total_process)
+   	print Process_array[i][0],'\t\t',Process_array[i][1],'\t\t',Process_array[i][2],'\t\t',Process_array[i][3],'\t\t',Process_array[i][4],'\t\t',Process_array[i][5]
    return
 
 Total_waiting_time, Total_process = inputProcess(Process_array,Total_process,Total_waiting_time)
 
+Process_array = Waiting_time(Process_array)
+
 showtabluarForm(Process_array,Total_process)
-
-PrintWaitingTime(Total_waiting_time,Total_process)
-    
-
-
-
